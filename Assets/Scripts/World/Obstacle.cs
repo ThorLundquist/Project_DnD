95eb44174ffb757838bf92c14e410237496d1a89
Thor Lundqvist <Lundquist.thor5@gmail.com>
2019-11-21 14:47:46 +0100

    Thursday 21st sep
    Worked on palm tree prefabs, gave them collider boxes and added script + interface to sort their layers. Their initial layer still has to be added manually, otherwise the character will clip through if touching two trees at once. This will probably not see a fix.

---
+++ Assets/Scripts/World/Obstacle.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour, IComparable<Obstacle>
+{
+
+    public SpriteRenderer MySpriteRenderer { get; set; }
+
+    private Color defaultColor;
+    private Color fadedColor;
+
+    public int CompareTo(Obstacle other)
+    {
+        if (MySpriteRenderer.sortingOrder > other.MySpriteRenderer.sortingOrder)
+        {
+            return 1;
+        }
+        else if (MySpriteRenderer.sortingOrder < other.MySpriteRenderer.sortingOrder)
+        {
+            return -1;
+        }
+
+        return 0;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MySpriteRenderer = GetComponent<SpriteRenderer>();
+
+        defaultColor = MySpriteRenderer.color;
+        fadedColor = defaultColor;
+        fadedColor.a = 0.7f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void FadeOut()
+    {
+        MySpriteRenderer.color = fadedColor;
+    }
+
+    public void FadeIn()
+    {
+        MySpriteRenderer.color = defaultColor;
+    }
+}
