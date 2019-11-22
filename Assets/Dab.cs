00520d043a3fd2e7533820f997ce3dbf34deda3c
Thor Lundqvist <Lundquist.thor5@gmail.com>
2019-11-22 14:23:25 +0100

    Friday 22nd attempt 2
    dabbed today, very important
    Did some fixex in the project folder structure. More to come.

---
+++ Assets/Dab.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dab : MonoBehaviour
+{
+    private bool flipstate;
+    private Vector3 poly;
+    // Start is called before the first frame update
+    void Start()
+    {
+        flipstate = false;
+        StartCoroutine("Flippy");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    IEnumerator Flippy()
+    {
+        while (true)
+        {
+            GetComponent<SpriteRenderer>().flipX = flipstate;
+            transform.localScale = new Vector3(-1, 1, 1);
+            flipstate = !flipstate;
+            yield return new WaitForSeconds(1f);
+        }
+    }
+}
