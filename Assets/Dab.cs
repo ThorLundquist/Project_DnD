9c26ed77fbc7641eee8987c0ffd9e4b54e8c69e2
Thor Lundqvist <Lundquist.thor5@gmail.com>
2019-11-25 14:36:09 +0100

    Monday 25th

--- Assets/Dab.cs
+++ Assets/Dab.cs
@@ -4,28 +4,59 @@ using UnityEngine;
 
 public class Dab : MonoBehaviour
 {
-    private bool flipstate;
-    private Vector3 poly;
+    private bool isFlipped;
+  //  private bool flipstate;
+  //  private Vector3 poly;
     // Start is called before the first frame update
     void Start()
     {
-        flipstate = false;
-        StartCoroutine("Flippy");
+        // flipstate = false;
+         StartCoroutine("Flip");
+
     }
 
     // Update is called once per frame
     void Update()
     {
-    }
-
-    IEnumerator Flippy()
-    {
-        while (true)
+        if (isFlipped)
         {
-            GetComponent<SpriteRenderer>().flipX = flipstate;
-            transform.localScale = new Vector3(-1, 1, 1);
-            flipstate = !flipstate;
-            yield return new WaitForSeconds(1f);
+            StartCoroutine("Flop");
+        }
+        else if (!isFlipped)
+        {
+            StartCoroutine("Flip");
+        }
+        else
+        {
+            Debug.Log("Fuck me");
         }
     }
+    IEnumerator Flip()
+    {
+        yield return new WaitForSeconds(1);
+        transform.localScale = new Vector3(-1, 1, 1);
+        isFlipped = true;
+        yield return new WaitForSeconds(1);
+    }
+    IEnumerator Flop()
+    {
+        yield return new WaitForSeconds(1);
+        transform.localScale = new Vector3(1, 1, 1);
+        isFlipped = false;
+        yield return new WaitForSeconds(1);
+    }
+    //IEnumerator Flippy()
+    //{
+
+    //    do
+    //    {
+    //        GetComponent<SpriteRenderer>().flipX = flipstate;
+    //        flipstate = !flipstate;
+
+
+    //        yield return new WaitForSeconds(1f);
+
+
+    //    } while (true);
+    //}
 }
