928b68adb90fbe2fab5a891992e1b459bea90efd
Thor Lundqvist <Lundquist.thor5@gmail.com>
2019-12-06 10:27:32 +0100

    Friday 6th dec
    Script to toggle UI done. Will work on the rest next week / weekend.

---
+++ Assets/InGameUI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameUI : MonoBehaviour
+{
+    public GameObject inventoryUI; // Assign in inspector
+    private bool isShowing;
+
+    void Start()
+    {
+        inventoryUI.SetActive(false);
+        isShowing = false;
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown("i"))
+        {
+            isShowing = !isShowing;
+            inventoryUI.SetActive(isShowing);
+        }
+    }
+
+    public void CloseButtonCalled()
+    {
+        isShowing = false;
+        inventoryUI.SetActive(false);
+    }
+}
