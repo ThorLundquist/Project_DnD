b52e35d1d45fe1d7d7bd5f4fb1d81d14ddd873da
Thor Lundqvist <Lundquist.thor5@gmail.com>
2019-12-10 14:47:15 +0100

    Tuesday 10th dec
    Did some more work on the UI. Some finishing touches on the MagicCircle script. Good stuff.

--- Assets/InGameUI.cs
+++ Assets/InGameUI.cs
@@ -4,26 +4,53 @@ using UnityEngine;
 
 public class InGameUI : MonoBehaviour
 {
-    public GameObject inventoryUI; // Assign in inspector
-    private bool isShowing;
+    public GameObject menuUI;
+    public GameObject inventoryUI;
+    public GameObject levelUpUI; 
+    // Assign these in the inspector
 
     void Start()
     {
         inventoryUI.SetActive(false);
-        isShowing = false;
+        menuUI.SetActive(false);
+        levelUpUI.SetActive(false);
     }
     void Update()
     {
         if (Input.GetKeyDown("i"))
         {
-            isShowing = !isShowing;
-            inventoryUI.SetActive(isShowing);
+            InventoryBtnClicked();
+        }
+
+        if (Input.GetKeyDown("escape"))
+        {            
+            MenuBtnClicked();
         }
     }
 
-    public void CloseButtonCalled()
+    public void MenuBtnClicked()
     {
-        isShowing = false;
         inventoryUI.SetActive(false);
+
+        if (menuUI.activeSelf)
+        {
+            menuUI.SetActive(false);
+        }
+        else if (menuUI.activeSelf == false)
+        {
+            menuUI.SetActive(true);
+        }
+    }
+
+    public void InventoryBtnClicked()
+    {
+        if (inventoryUI.activeSelf)
+        {
+            inventoryUI.SetActive(false);
+        }
+        else if (inventoryUI.activeSelf == false && menuUI.activeSelf == false)
+        {
+            inventoryUI.SetActive(true);
+        }
     }
 }
--- Assets/Scripts/World/MagicCircle.cs
+++ Assets/Scripts/World/MagicCircle.cs
@@ -3,57 +3,68 @@
 public class MagicCircle : MonoBehaviour
 {
     public bool move = false;
-    public Vector2 mcPos;
+    public bool isInside = false;
     public Vector2 cPos;
     public Vector2 cenPos;
     Animator anim;
-
     /*
         I've found two ways to go about getting the position of any object in Unity. First is to tag it in the editor. 
         The other is to reference it as an object in a script, like I've done here
     */
+    public GameObject floatingText;
     public GameObject center;
-    public GameObject character; // You will need to drag the character object in the Unity editor's Hierarchy onto the Script in the Character field
+    public GameObject character; // You will need to drag the objects in the Unity editor's Hierarchy onto the Script in the given field
 
 
     private void Start()
     {
-        cenPos = center.transform.position;
-        // mcPos = GameObject.FindGameObjectWithTag("MC").transform.position;  
-        // Takes the position of any object with the tag "MC" and stores it in the variable "mcPos"
-
+        floatingText.SetActive(false);
         anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        cenPos = center.transform.position;
         cPos = character.transform.position; //Catches Character's position and puts it in the variable "cPos"
-        if (move)
-        {
-            // If Character's collider triggers MagicCircle's collider, character moves into the middle of MagicCircle
-            transform.position = Vector2.MoveTowards(transform.position, cenPos, 2 * Time.deltaTime); 
 
-            if (cenPos == cPos)
+        if (move)
+        {           
+            // isInside = true;
+            Debug.Log("moving");
+            // If Character's collider triggers MagicCircle's collider, character moves into the middle of MagicCircle
+            transform.position = Vector2.MoveTowards(cPos, cenPos, 2 * Time.deltaTime);
+            if (cPos == cenPos)
             {
                 move = false;
-                Debug.Log("it should work");  
-
-              //  if (Input.GetButtonDown("Test"))
-               // {
-                    Debug.Log("Press space to teleport!");
-                    anim.SetTrigger("isTeleport");
-              //  }
+                isInside = true;
+                floatingText.SetActive(true);
             }
         }
 
+        if (isInside && Input.GetKeyDown("h"))
+        {
+            Debug.Log("TP!");
+        }
     }
 
     void OnTriggerEnter2D(Collider2D col) // this calls when Character collider box touches MagicCircle Collider box
     {
         if (col.tag == "MC")
         {
-            move = true;
+            Debug.Log("In");
+            move = true;            
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.tag == "MC")
+        {
+            Debug.Log("Out");
+            isInside = false;
+            move = false;
+            floatingText.SetActive(false);
         }
     }
 }
